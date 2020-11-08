8cfc160410b8579fbf79d87d69432fa6129b379b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-08 04:48:55 +0000

    Deploy dev-launchers/platform__website to github.com/dev-launchers/platform__website.git:gh-pages

---
+++ precache-manifest.59209e5ef1e5bb8d521ac041a568e790.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84913bfb818fc5dbbf804ae921115927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c460313a6892e5d9bd62",
+    "url": "/static/css/2.fa079e88.chunk.css"
+  },
+  {
+    "revision": "b4ca36946e1bf7060b2e",
+    "url": "/static/css/main.e45ffc3b.chunk.css"
+  },
+  {
+    "revision": "c460313a6892e5d9bd62",
+    "url": "/static/js/2.59f405dd.chunk.js"
+  },
+  {
+    "revision": "fc1b801c5bed73138b55d83eddf4f104",
+    "url": "/static/js/2.59f405dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ca36946e1bf7060b2e",
+    "url": "/static/js/main.56deacf1.chunk.js"
+  },
+  {
+    "revision": "a7ff2af6ba9544408944",
+    "url": "/static/js/runtime-main.6e5b6221.js"
+  },
+  {
+    "revision": "09dda5275a8b7690fa3cffc98cb2248e",
+    "url": "/static/media/adults-coding.09dda527.jpg"
+  },
+  {
+    "revision": "00bb64acb54eacf5acfc3c2f4c765d78",
+    "url": "/static/media/chess_display_large.00bb64ac.jpg"
+  },
+  {
+    "revision": "9c884c33e5dc1a679a65ba8b333bc20d",
+    "url": "/static/media/hero-render.9c884c33.png"
+  },
+  {
+    "revision": "8a116df5c2d6c7739f6964f63c57f851",
+    "url": "/static/media/level-0.8a116df5.png"
+  },
+  {
+    "revision": "4a12b88d50437ecd13af3d44337e3f55",
+    "url": "/static/media/level-1.4a12b88d.png"
+  },
+  {
+    "revision": "1873c353b8ea0c7bc73089a36a82eed2",
+    "url": "/static/media/level-2.1873c353.png"
+  },
+  {
+    "revision": "a4ac46c79fb70de81467116f6e01b0b3",
+    "url": "/static/media/level-3.a4ac46c7.png"
+  },
+  {
+    "revision": "6d65840c7d794c84634ec9e30e1c062f",
+    "url": "/static/media/level-4.6d65840c.png"
+  },
+  {
+    "revision": "c51e1e319cd2d41cea9e351a49f60007",
+    "url": "/static/media/level-5.c51e1e31.png"
+  },
+  {
+    "revision": "aac41382f1b43cb6032f7d72eef60a78",
+    "url": "/static/media/level-6.aac41382.png"
+  },
+  {
+    "revision": "d1d87c1ea5728c0d06aa1cbd37265bcf",
+    "url": "/static/media/level-7.d1d87c1e.png"
+  },
+  {
+    "revision": "012d29740e50b32c1ec815b8c847b309",
+    "url": "/static/media/level-8.012d2974.png"
+  },
+  {
+    "revision": "69c48499d5db01a77a3d8e009fde3b0a",
+    "url": "/static/media/logo-monogram.69c48499.png"
+  },
+  {
+    "revision": "b1545104ade30950db8f56d67b38361b",
+    "url": "/static/media/people-inside-room-3184468.b1545104.jpg"
+  },
+  {
+    "revision": "3371265e80d69fc1a32f576a805c472c",
+    "url": "/static/media/startup-table.3371265e.jpg"
+  }
+]);
